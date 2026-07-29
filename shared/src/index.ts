32c234ff11af86038de2e83a392c5f4134802d40
Chinonso Oti <188906421+chinooti@users.noreply.github.com>
2026-07-29 20:10:21 +0100

    Revert "Feat/card types"

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -2,5 +2,3 @@ export * from './types/primitives';
 export * from './types/keyword';
 export * from './types/effect';
 export * from './types/ability';
-export * from './types/card';
-export * from './types/instance';
